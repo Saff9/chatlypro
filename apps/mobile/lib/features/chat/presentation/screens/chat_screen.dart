82de34cf0c72c0a53b308d317324782de6cbcfc5
Saff9 <saffanakbar942@gmail.com>
2026-06-12 17:46:21 +0530

    Remove unused cryptography import in chat_screen.dart to fix analyzer warning

--- apps/mobile/lib/features/chat/presentation/screens/chat_screen.dart
+++ apps/mobile/lib/features/chat/presentation/screens/chat_screen.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:cryptography/cryptography.dart';
 import 'package:hive/hive.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'chat_list_screen.dart';
